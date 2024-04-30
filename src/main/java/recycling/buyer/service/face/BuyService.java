5460c0b2990bc937da1708a0bce3f11971f0e0bb
Kim Hyun Jeong <guswjdrla120@gmail.com>
2024-04-30 22:54:30 +0900

    Update Login

--- src/main/java/recycling/buyer/service/face/BuyService.java
+++ src/main/java/recycling/buyer/service/face/BuyService.java
@@ -1,7 +1,11 @@
 package recycling.buyer.service.face;
 
+import recycling.dto.buyer.Buyer;
+
 // 메인페이지, 로그인/회원가입
 
 public interface BuyService {
+
+	public Buyer selectByIdPw(Buyer buyer);
 
 }
--- src/main/java/recycling/buyer/service/impl/BuyServiceImpl.java
+++ src/main/java/recycling/buyer/service/impl/BuyServiceImpl.java
@@ -2,12 +2,21 @@ package recycling.buyer.service.impl;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import recycling.buyer.dao.face.BuyDao;
 import recycling.buyer.service.face.BuyService;
+import recycling.dto.buyer.Buyer;
 
 @Service
 public class BuyServiceImpl implements BuyService {
 	
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Autowired BuyDao buyDao;
+	
+	@Override
+	public Buyer selectByIdPw(Buyer buyer) {
+		return buyDao.selectByIdPW(buyer);
+	}
 }
