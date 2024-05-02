1c9e5ca6f6b38f8463e99f33178e230ecfebd343
DESKTOP-TNJQ777\user1 <guswjdrla120@gmail.com>
2024-05-02 10:44:46 +0900

    commit

--- src/main/java/recycling/buyer/controller/BuyerController.java
+++ src/main/java/recycling/buyer/controller/BuyerController.java
@@ -1,8 +1,12 @@
 package recycling.buyer.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 // 마이페이지 - 회원 정보 관련
@@ -12,6 +16,5 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BuyerController {
 	
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
-	
 	
 }
