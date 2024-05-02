26227b05b94e1baa234418519d2baca8560b7cc2
DESKTOP-2B5EAKD\user1 <girlfriend96@naver.com>
2024-05-02 11:29:46 +0900

    test

--- src/main/java/recycling/buyer/service/face/ExpService.java
+++ src/main/java/recycling/buyer/service/face/ExpService.java
@@ -1,5 +1,14 @@
 package recycling.buyer.service.face;
 
+import java.util.List;
+
+import recycling.dto.seller.Exp;
+import recycling.util.Paging;
+
 public interface ExpService {
+
+	Paging getPaging(int curPage);
+
+	List<Exp> selectAllExp(Paging paging);
 
 }
