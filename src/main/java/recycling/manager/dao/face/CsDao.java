e9ada76943358dec5adacfed682bd6bc56a68f01
DESKTOP-JGVLFKT\user1 <kiasl1024@naver.com>
2024-05-03 16:18:40 +0900

    commit

--- src/main/java/recycling/manager/dao/face/CsDao.java
+++ src/main/java/recycling/manager/dao/face/CsDao.java
@@ -1,7 +1,35 @@
 package recycling.manager.dao.face;
 
-// 구매CS팀 DB 처리
+import java.util.List;
 
+import recycling.dto.buyer.Buyer;
+import recycling.dto.buyer.Oto;
+import recycling.util.Paging;
+
+// 구매CS팀 DB 처리
 public interface CsDao {
+
+	/**
+	 * 구매자 문의글 조회
+	 * 
+	 * @param paging
+	 * @return
+	 */
+	public List<Oto> selectAllOto(Paging paging);
+
+	/**
+	 * 총 게시글 조회
+	 * 
+	 * @return
+	 */
+	public int selectCntAll();
+
+	/**
+	 * 모든 구매자 조회
+	 * 
+	 * @param paging
+	 * @return
+	 */
+	public List<Buyer> selectAllBuyer(Paging paging);
 
 }
--- src/main/java/recycling/manager/service/impl/CsServiceImpl.java
+++ src/main/java/recycling/manager/service/impl/CsServiceImpl.java
@@ -1,13 +1,45 @@
 package recycling.manager.service.impl;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import recycling.dto.buyer.Buyer;
+import recycling.dto.buyer.Oto;
+import recycling.manager.dao.face.CsDao;
 import recycling.manager.service.face.CsService;
+import recycling.util.Paging;
 
 @Service
 public class CsServiceImpl implements CsService {
 	
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	private CsDao csDao;
+
+	@Override
+	public List<Oto> list(Paging paging) {
+		return csDao.selectAllOto(paging);
+	}
+
+	@Override
+	public Paging getPaging(Paging pagingParam) {
+
+		// 총 게시글 수 조회
+		int totalCount = csDao.selectCntAll();
+
+		// 페이징 계산
+		Paging paging = new Paging(totalCount, pagingParam.getCurPage(), pagingParam.getSearch());
+
+		return paging;
+	}
+
+	@Override
+	public List<Buyer> buyerList(Paging paging) {
+		return csDao.selectAllBuyer(paging);
+	}
 }
