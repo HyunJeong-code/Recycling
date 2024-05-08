b2436f3422cd2fc851f9ec92b84de8702a01686d
Kim Hyun Jeong <guswjdrla120@gmail.com>
2024-05-08 23:18:32 +0900

    Updating Login Process

--- src/main/java/recycling/buyer/service/impl/BuyServiceImpl.java
+++ src/main/java/recycling/buyer/service/impl/BuyServiceImpl.java
@@ -3,14 +3,18 @@ package recycling.buyer.service.impl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import recycling.buyer.dao.face.BuyDao;
 import recycling.buyer.service.face.BuyService;
 import recycling.dto.buyer.Buyer;
+import recycling.dto.buyer.BuyerLogin;
 
 @Service
-public class BuyServiceImpl implements BuyService {
+public class BuyServiceImpl implements BuyService, UserDetailsService {
 	
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Autowired BuyDao buyDao;
@@ -18,5 +22,11 @@ public class BuyServiceImpl implements BuyService {
 	@Override
 	public Buyer selectByIdPw(Buyer buyer) {
 		return buyDao.selectByIdPW(buyer);
+	}
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//		BuyerLogin buyerLogin = buyDao.selectById(username);
+		return null;
 	}
 }
