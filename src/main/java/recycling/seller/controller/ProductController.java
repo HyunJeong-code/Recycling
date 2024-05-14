7f06449fb2cce613bbd9cfe1cca04f196950b64b
Kim Hyun Jeong <guswjdrla120@gmail.com>
2024-05-14 14:17:46 +0900

    Update Prd

--- src/main/java/recycling/seller/controller/ProductController.java
+++ src/main/java/recycling/seller/controller/ProductController.java
@@ -1,9 +1,22 @@
 package recycling.seller.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import recycling.dto.buyer.BuyerLogin;
+import recycling.dto.seller.Prd;
+import recycling.dto.seller.PrdFile;
+import recycling.seller.service.face.ProductService;
 
 // 상품 등록
 
@@ -12,4 +25,65 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProductController {
 	
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Autowired private ProductService productService;
+	
+	@GetMapping("/main")
+	public void main() {
+		logger.info("/seller/prd/main [GET]");
+	}
+	
+	@GetMapping("/rcyform")
+	public void rcyForm() {
+		logger.info("/seller/prd/rcyform [GET]");
+	}
+	
+	@PostMapping("/rcyform")
+	public void rcyFormProc(HttpSession session, Prd prd, MultipartFile main, List<MultipartFile> detail) {
+		logger.info("/seller/prd/rcyform [POST]");
+		
+		BuyerLogin seller = (BuyerLogin) session.getAttribute("buyers"); 
+		prd.setsCode(seller.getsCode());
+		logger.info("rcy : {}", prd);
+		
+		int res = productService.insertRcy(prd);
+		
+		logger.info("main : {}", main);
+		for(MultipartFile mult : detail) {
+			logger.info("detail : {}", mult);			
+		}
+		
+		if(res > 0) {
+			PrdFile prdMain = productService.saveFile(main, prd);
+			if(prdMain != null) {
+				int resMain = productService.insertFileMain(prdMain);
+				
+				int resDetail = 0;
+				for(MultipartFile mult : detail) {
+					
+					PrdFile prdDetail = productService.saveFile(mult, prd);
+					if(prdDetail != null) {
+						resDetail += productService.insertFileDetail(prdDetail);						
+					}
+				}
+				
+				if(resMain > 0 && resDetail == detail.size()) {
+					// 상품 상세페이지로 이동
+				} else {
+					// 상품 등록 전체 삭제
+				}
+			}
+		} else {
+			
+		}
+	}
+	
+	@GetMapping("/upcyform")
+	public void upcyForm() {
+		logger.info("/seller/prd/upyform [GET]");
+	}
+	
+	@PostMapping("/upcyform")
+	public void upcyFormProc() {
+		logger.info("/seller/prd/upyform [POST]");
+	}
 }
--- src/main/java/recycling/seller/service/impl/ProductServiceImpl.java
+++ src/main/java/recycling/seller/service/impl/ProductServiceImpl.java
@@ -1,10 +1,86 @@
 package recycling.seller.service.impl;
 
-import org.springframework.stereotype.Service;
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
+import javax.servlet.ServletContext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import recycling.dto.seller.Prd;
+import recycling.dto.seller.PrdFile;
+import recycling.seller.dao.face.ProductDao;
 import recycling.seller.service.face.ProductService;
 
 @Service
 public class ProductServiceImpl implements ProductService {
-
+	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Autowired private ProductDao productDao;
+	@Autowired private ServletContext servletContext;
+	
+	@Override
+	public int insertRcy(Prd prd) {
+		return productDao.insertRcy(prd);
+	}
+	
+	@Override
+	public PrdFile saveFile(MultipartFile file, Prd prd) {
+		
+		if(file.getSize() <= 0) {
+			logger.info("파일 없음");
+			
+			return null;
+		}
+		
+		String storedPath = servletContext.getRealPath("upload");
+		
+		File storedFolder = new File(storedPath);
+		storedFolder.mkdir();
+		
+		String storedName = null;
+		
+		File dest = null;
+		
+		do {
+			storedName = file.getOriginalFilename(); // 원본 파일명
+			
+			storedName += UUID.randomUUID().toString().split("-")[4]; // UUID 추가
+			logger.info("storedName : {}", storedName);
+			
+			dest = new File(storedFolder, storedName);			
+		} while(dest.exists());
+		
+		try {
+			file.transferTo(dest);
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		PrdFile prdFile = new PrdFile();
+		prdFile.setPrdCode(prd.getPrdCode());
+		prdFile.setOriginName(file.getOriginalFilename());
+		prdFile.setStoredName(storedName);
+		
+		return prdFile;
+	}
+	
+	@Override
+	public int insertFileMain(PrdFile prdFile) {
+		prdFile.setCtPflNo(600);;
+		return productDao.insertPrdFile(prdFile);
+	}
+	
+	@Override
+	public int insertFileDetail(PrdFile prdDetail) {
+		prdDetail.setCtPflNo(610);
+		return productDao.insertPrdFile(prdDetail);
+	}
 }
