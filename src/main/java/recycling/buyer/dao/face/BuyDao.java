755fc0ae3514c51f4c6224aa6dc3b6e673f7587c
Jeong Hoon Kim <jhblue98@naver.com>
2024-05-24 16:03:49 +0900

    test

--- src/main/java/recycling/buyer/dao/face/BuyDao.java
+++ src/main/java/recycling/buyer/dao/face/BuyDao.java
@@ -1,7 +1,16 @@
 package recycling.buyer.dao.face;
 
+import java.util.List;
+import java.util.Map;
+
 import recycling.dto.buyer.Buyer;
+import recycling.dto.buyer.BuyerAdr;
 import recycling.dto.buyer.BuyerLogin;
+import recycling.dto.buyer.BuyerProf;
+import recycling.dto.buyer.BuyerTest;
+import recycling.dto.buyer.Buyers;
+import recycling.dto.buyer.Cmp;
+import recycling.dto.buyer.CmpFile;
 
 // 메인페이지, 로그인/회원가입 관련 DB 처리
 
@@ -30,5 +39,67 @@ public interface BuyDao {
 	 * @return - null : 실패, else : 성공
 	 */
 	public BuyerLogin selectBybIdbPw(Buyer buyer);
+
+	/**
+	 * 아이디 찾기
+	 * 
+	 * @param buyer - 아이디 찾을 회원 정보
+	 * @return 아이디 반환
+	 */
+	public String selectByBuyerId(Buyer buyer);
 	
+	/**
+	 * 비밀번호 찾기
+	 * 
+	 * @param buyer - 비밀번호를 찾을 회원 정보
+	 * @return true : 회원 정보 존재, false : 회원정보 존재 X
+	 */
+	public Buyer selectByBuyerPw(Buyer buyer);
+	
+	/**
+	 * 비밀번호 찾기 후 변경
+	 * 
+	 * @param buyer - 회원 코드와 변경할 비밀번호
+	 * @return 0 : 실패, 1 : 성공
+	 */
+	public int updatePw(Buyer buyer);
+
+	/**
+	 * 프로필 사진 삽입
+	 * 
+	 * @param prof - 프로필 사진 정보
+	 * @return 0 : 실패, 1 : 성공
+	 */
+	public int insertProf(BuyerProf prof);
+	
+	/**
+	 * 구매자 주소 정보 삽입
+	 * @param buyerAdr - 구매자 주소 정보
+	 * @return 0 : 실패, 1 : 성공
+	 */
+	public int insertAdr(BuyerAdr buyerAdr);
+	
+	/**
+	 * 기업 구매자 정보 삽입
+	 * 
+	 * @param cmp - 기업 정보
+	 * @return 0 : 실패, 1 : 성공
+	 */
+	public int insertCmp(Cmp cmp);
+	
+	/**
+	 * 기업 구매자 서류 삽입
+	 * 
+	 * @param file - 서류 파일 정보
+	 * @return 0 : 실패, 1 : 성공
+	 */
+	public int insertCmpFile(CmpFile file);
+	
+	/**
+	 * 헤더 공지
+	 * 
+	 * @return map형태로 번호와 제목
+	 */
+	public List<Map<String, Object>> selectNtc();
+
 }
--- src/main/webapp/WEB-INF/views/buyer/main.jsp
+++ src/main/webapp/WEB-INF/views/buyer/main.jsp
@@ -1,28 +1,22 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>새활용</title>
+<title>새활용 메인페이지</title>
 </head>
 <body>
+<c:import url="/WEB-INF/views/layout/buyer/buyerheader.jsp"/>
 
 <div class="full">
-<sec:authorize access="isAnonymous()">
-<button><a href="./login">로그인</a></button>
-</sec:authorize>
-
-<sec:authorize access="hasRole('ROLE_BUYER')">
-<button><a href="./logout">로그아웃</a></button>
-</sec:authorize>
 	<div class="wrap">
 	
 		<!-- 임시설정 -->
 		<a href="/buyer/login">로그인</a><br>
 		<a href="/buyer/mypage/mymain">마이페이지</a><br>
-		<a href="/buyer/mypage/changepw">비밀번호 변경</a><br>
 		<a href="/buyer/mypage/mydetailpri">개인 회원 정보 변경</a><br>
 		<a href="/buyer/mypage/mydetailcmp">기업 회원 정보 변경</a><br>
 		<a href="/buyer/mypage/myaddr">배송지</a><br>
