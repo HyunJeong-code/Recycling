ae684e76db67093908559a712b60e0c0719e14d4
DESKTOP-NE1HP2P\user1 <wkdghk7875@naver.com>
2024-05-27 10:13:47 +0900

    pull main

--- src/main/java/recycling/buyer/controller/RecyclingController.java
+++ src/main/java/recycling/buyer/controller/RecyclingController.java
@@ -1,30 +1,18 @@
 package recycling.buyer.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import recycling.buyer.service.face.RecyclingService;
-import recycling.dto.buyer.Buyer;
-import recycling.dto.seller.Seller;
-import recycling.util.Paging;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
