f8931e8407db04a83ca70ccdcee005538b7eb237
DESKTOP-EB2TAVS\user1 <jhblue98@naver.com>
2024-05-30 12:09:00 +0900

    test

--- src/main/java/recycling/buyer/service/impl/MypageServiceImpl.java
+++ src/main/java/recycling/buyer/service/impl/MypageServiceImpl.java
@@ -7,24 +7,38 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import recycling.buyer.dao.face.MypageDao;
 import recycling.buyer.service.face.MypageService;
 import recycling.dto.buyer.BuyerLogin;
+import recycling.util.PagingAndCtg;
 
 @Service
+@Transactional
 public class MypageServiceImpl implements MypageService {
 	
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Autowired private MypageDao mypageDao;
 	
 	@Override
-	public List<Map<String, Object>> selectQnaBybCode(BuyerLogin buyerLogin) {
-		return mypageDao.selectQnaBybCode(buyerLogin);
+	public List<Map<String, Object>> selectQnaBybCode(PagingAndCtg paging) {
+		return mypageDao.selectQnaBybCode(paging);
 	}
 	
 	@Override
-	public List<Map<String, Object>> selectRvwBybCode(BuyerLogin buyerLogin) {
-		return mypageDao.selectRvwBybCode(buyerLogin);
+	public List<Map<String, Object>> selectRvwBybCode(PagingAndCtg paging) {
+		return mypageDao.selectRvwBybCode(paging);
+	}
+	
+	@Override
+	public int selectCntQna(PagingAndCtg upPaging) {
+		return mypageDao.selectCntQna(upPaging);
+
+	}
+	
+	@Override
+	public int selectCntRvw(PagingAndCtg unPaging) {
+		return mypageDao.selectCntRvw(unPaging);
 	}
 }
